9c1bdf7d96f5ba8ec19e01781e1ae4a244c15e2b
Joresgon <jo.reyesgon@gmail.com>
2021-12-22 20:22:14 -0300

    some changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import React, { useState } from "react";
 import Todo from "./components/Todo";
+import TodoForm from "./components/TodoFrom";
 
 function App() {
+  const [todos, setTodos] = useState ([]);
+
+  const addTodo = (todo) => {
+    setTodos([todo, ...todos])
+  }
+
+
   return (
     <div className="App">
-      <Todo />
+      <TodoForm addtodo={addTodo}/>
     </div>
   );
 }
--- src/components/TodoFrom.js
+++ src/components/TodoFrom.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
+import uuid from "uuid";
 
-const TodoForm = () => {
+const TodoForm = ({ addTodo }) => {
     const [todo, setTodo] = useState({
         id: "",
         task: "",
@@ -9,10 +10,19 @@ const TodoForm = () => {
 
     const handleTaskInputChange = (e) => {
         setTodo({ ...todo, task: e.target.value });
-    }
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (todo.task.trim()) {
+            addTodo({ ...todo, id: uuid.v4() })
+            // reset task input
+            setTodo({ ...todo, task:"" });
+        };
+    };
 
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <input name="task" type="text" value={todo.task} onChange={handleTaskInputChange} />
             <button type="submit"/>
         </form>
