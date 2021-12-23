189f53e92a82ac807c53c29158465ebe9dbd935e
Joresgon <jo.reyesgon@gmail.com>
2021-12-23 01:12:47 -0300

    new repo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
-import React, { useState, useEffect } from "react";
-import "../src/App.css"
-import TodoForm from "./components/TodoFrom";
-import TodoList from "./components/TodoList";
-
-const LOCAL_STORAGE_KEY = "react-todo-list-todos";
+import TodoList from './components/TodoList'
 
 function App() {
-  const [todos, setTodos] = useState ([]);
-
-  useEffect(() => {
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
-  }, [todos]);
-
-  const addTodo = (todo) => {
-    setTodos([todo, ...todos]);
-  };
-
   return (
     <div className="App">
-      <p>React Todo</p>
-      <TodoForm addTodo={addTodo}/>
-      <TodoList todos={todos} />
+      <TodoList />
     </div>
   );
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,14 +1,41 @@
-import React from 'react';
-import Todo from './Todo';
+import React, { useState } from 'react';
+import '../App.css';
 
-const TodoList = ({todos}) => {
-   return(
-       <ul>
-            {todos.map(todo => (
-                <Todo key={todo.id} todo={todo} />
-            ))}
-        </ul>
-    )
-};
+const TodoList = () => {
+    const [listTask, setListTask] = useState([]);
+    const [task, setTask] = useState('');
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        setListTask([...listTask, task]);
+    };
+    const handleChange = (e) => {
+        setTask(e.target.value);
+    };
+    const deleteTask = (key) => {
+        setListTask(listTask.filter((item, index) => index !== key));
+        console.log(key);
+    };
+
+    return (
+        <div className="todo-list">
+            <h1>Todo List</h1>
+            <div className="container">
+                <form onSubmit={onSubmit}>
+                    <div className="col-md-8 mx-auto" >
+                        <div className="input-group">
+                            <input type="text" className="form-control" value={task} onChange={handleChange} placeholder="Enter a task" />
+                        </div>
+                        <ul className="list-group">
+                            {
+                                listTask.map((li, key) => <li className="list-group-item" key={key} >{li}<span className="close" onClick={() => deleteTask(key)}>X</span></li>)
+                            }
+                        </ul>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+}
 
 export default TodoList;
